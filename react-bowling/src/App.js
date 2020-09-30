d342f890dd7453bbf077762fd14b74dda4fe402d
callor <callor@callor.com>
2020-09-30 23:14:51 +0900

    logo

--- react-bowling/src/App.js
+++ react-bowling/src/App.js
@@ -6,10 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span className="App-logo" aria-label="jsx-a11y/accessible-emoji">
-          🎳
-        </span>
-        <h3>빛나라&nbsp;&nbsp;&nbsp;볼링대전&nbsp;&nbsp;&nbsp;2020에&nbsp;&nbsp;&nbsp;오신것을&nbsp;&nbsp;환경합니다!!!</h3>
+        <img
+          src={process.env.PUBLIC_URL + "/bowl-logo.png"}
+          className="App-logo"
+        />
+
+        <h3>
+          빛나라&nbsp;&nbsp;&nbsp;볼링대전&nbsp;&nbsp;&nbsp;2020에&nbsp;&nbsp;&nbsp;오신것을&nbsp;&nbsp;환경합니다!!!
+        </h3>
         <StartGame />
       </header>
     </div>
