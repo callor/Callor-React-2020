12456475ea445c05a9b9b465ccbd397f9a8b9bf8
callor <callor@callor.com>
2020-12-31 09:48:57 +0900

    B

---
+++ react-typescript/build/precache-manifest.879876858324cc4315f805dffd21abf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e810562e970db84be876451e8b339a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fed2a6120dfb0e385ac",
+    "url": "/static/css/main.463f1b69.chunk.css"
+  },
+  {
+    "revision": "2ba9ef2165c9a7e0ba68",
+    "url": "/static/js/2.4b72cea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b72cea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fed2a6120dfb0e385ac",
+    "url": "/static/js/main.23ecab3f.chunk.js"
+  },
+  {
+    "revision": "52b12994057cb9471f31",
+    "url": "/static/js/runtime-main.02c0d6d6.js"
+  }
+]);
