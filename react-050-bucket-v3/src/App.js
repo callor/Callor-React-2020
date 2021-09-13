bf550d8424ed4c6f92068a7be7a4b2e04985d259
callor <callor@callor.com>
2021-09-13 17:48:42 +0900

    bucket

--- react-050-bucket-v3/src/App.js
+++ react-050-bucket-v3/src/App.js
@@ -5,39 +5,39 @@ import BucketMain from "./main/BucketMain";
 // import { useEffect } from "react";
 
 function App() {
-  // const socket = socketio("http://localhost:12700", {
-  //   transports: ["websocket"],
-  //   path: "/", // added this line of code
-  // });
+    // const socket = socketio("http://localhost:12700", {
+    //   transports: ["websocket"],
+    //   path: "/", // added this line of code
+    // });
 
-  // useEffect(() => {
-  //   socket.on("connect", () => {
-  //     console.log("connected");
-  //   });
-  //   socket.emit("init", "Hello");
+    // useEffect(() => {
+    //   socket.on("connect", () => {
+    //     console.log("connected");
+    //   });
+    //   socket.emit("init", "Hello");
 
-  //   socket.on("event", (data) => {
-  //     console.log(data);
-  //   });
-  // });
+    //   socket.on("event", (data) => {
+    //     console.log(data);
+    //   });
+    // });
 
-  const background = {
-    backgroundImage: `url(${Background})`,
-    backgroundRepeat: "no-repeat",
-    backgroundAttachment: "fixed",
-    backgroundSize: "100% 100vh",
-  };
-  return (
-    <div className="App">
-      <header className="App-header" style={background}>
-        <h2>MY BUCKET LIST</h2>
-      </header>
-      <BucketMain />
-      <footer className="footer">
-        <address>CopyRight &copy; callor@callor.com</address>
-      </footer>
-    </div>
-  );
+    const background = {
+        backgroundImage: `url(${Background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "scroll",
+        backgroundSize: "contain",
+    };
+    return (
+        <div className="App">
+            <header className="App-header" style={background}>
+                <h1>MY BUCKET LIST</h1>
+            </header>
+            <BucketMain />
+            <footer className="footer">
+                <address>CopyRight &copy; callor@callor.com</address>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
