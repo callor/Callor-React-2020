108a66cc19ffadd4de35ca1d597a9f0f39f04c39
callor <callor@callor.com>
2021-09-29 08:14:19 +0900

    합성

--- react-2021-040-context-v1/src/App.js
+++ react-2021-040-context-v1/src/App.js
@@ -2,28 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import MessageContext from "./context/MessageContext";
-
-import MessageUpdate from "./comps/MessageUpdate";
-import MessageView from "./comps/MessageView";
+import MessageMain from "./context/MessageMain";
 
 function App() {
-    const [phone, setPhone] = useState({
-        p_id: 1,
-        p_name: "",
-        p_tel: "",
-        p_email: "",
-    });
-
-    const [phoneBook, setPhoneBook] = useState([]);
-
     const [message, setMessage] = useState("전달");
     const [isEdit, setEdit] = useState(false);
     // Provider에 JSON객체 type으로 묶어서 전달할 것
     const providorData = {
-        phone,
-        setPhone,
-        phoneBook,
-        setPhoneBook,
         message,
         setMessage,
         isEdit,
