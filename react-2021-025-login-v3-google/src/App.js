9cc1bc7766452cdeeb9aa5f1a5856c9e5997aa9a
callor <callor@callor.com>
2021-10-12 13:58:45 +0900

    R

--- react-2021-025-login-v3-google/src/App.js
+++ react-2021-025-login-v3-google/src/App.js
@@ -1,14 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import GoogleButton from "./custom/GoogleButton";
+import GoogleLogoutButton from "./custom/GoogleLogoutButton";
 
 function App() {
+    const [googleUser, setGoogleUser] = useState("");
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
-            <GoogleButton />
+            <GoogleButton
+                googleUser={googleUser}
+                setGoogleUser={setGoogleUser}
+            />
+            <GoogleLogoutButton setGoogleUser={setGoogleUser} />
         </div>
     );
 }
--- react-2021-025-login-v3-google/src/custom/GoogleButton.jsx
+++ react-2021-025-login-v3-google/src/custom/GoogleButton.jsx
@@ -1,84 +1,106 @@
 import "../css/GoogleButton.css";
 import { useEffect, useRef, useState } from "react";
 
-function GoogleButton() {
+function GoogleButton({ googleUser, setGoogleUser }) {
     const googleLoginBtn = useRef(null);
-    const [auth2, setAuth2] = useState("");
-    const [googleUser, setGoogleUser] = useState("");
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = `https://apis.google.com/js/platform.js?onload=renderButton`;
-        script.type = "text/javascript";
-        script.async = "defer";
-        document.head.appendChild(script);
-    }, []);
+    // const [googleUser, setGoogleUser] = useState("");
+    // useEffect(() => {
+    //     const script = document.createElement("script");
+    //     script.src = `https://apis.google.com/js/platform.js?onload=renderButton`;
+    //     script.type = "text/javascript";
+    //     script.async = "defer";
+    //     document.head.appendChild(script);
+    // }, []);
 
-    const googleOkReturn = (user) => {
+    const googleOkReturn = (googleUser) => {
         // 로그인한 사용자 정보를 googleUser 정보에 저장
-        setGoogleUser(user.getBasicProfile());
-        console.log("getProfile");
-        const profile = user.getBasicProfile();
-        console.log(profile);
-        console.log(`Token || ${user.getAuthResponse().id_token}`);
+
+        const profile = googleUser.getBasicProfile();
+        console.log("profile", profile);
+        console.log(`Token || ${googleUser.getAuthResponse().id_token}`);
         console.log(`ID: ${profile.getId()}`);
         console.log(`Name: ${profile.getName()}`);
         console.log(`Image URL: ${profile.getImageUrl()}`);
         console.log(`Email: ${profile.getEmail()}`);
+
+        const user = {
+            userid: profile.getId(),
+            name: profile.getName(),
+            image: profile.getImageUrl(),
+            email: profile.getEmail(),
+        };
+        setGoogleUser(user);
     };
 
-    const googleSDK_init = async () => {
-        console.log("gapi", window.gapi);
+    const googleSDK_init = () => {
+        // console.log("gapi", window.gapi);
         /**
          * windows.gapi 를 사용하기 위하여 index.html에 다음 스크립트를 include 한다
          * script src="https://apis.google.com/js/api:client.js"
          */
+        if (!window.gapi) {
+            alert("google API Not found");
+            return;
+        }
         window.gapi.load("auth2", async () => {
             // auth2 = GoogleAuth type 객체
             const auth2 = await window.gapi.auth2.init({
                 client_id:
                     "914378613858-5h170in8llkq1i3gdh8ugg4bontble3f.apps.googleusercontent.com",
+                scope: "profile email",
             });
-            console.log("auth2", auth2);
+            // console.log("auth2", auth2);
             if (auth2.isSignedIn.get()) {
                 console.log("로그인 된 상태");
-                // 로그인이 확인되면 auth 정보를 state에 담아둔다
-                auth2.attachClickHandler("");
-                setAuth2(auth2);
                 const user = auth2.currentUser.get();
-                setGoogleUser(user);
-            } else {
-                console.log("로그인 되지 않은 상태");
-                // 로그인이 되어 있지 않을 경우 구글 로그인 창 호출하기
-                // attachClickHandler(container,paramsOption,onSuccess, onFailure);
-                await auth2.attachClickHandler(
-                    googleLoginBtn.current,
-                    {},
-                    googleOkReturn,
-                    (err) => alert(JSON.stringify(err, undefined, 2))
-                );
+                googleOkReturn(user);
             }
+            await auth2.attachClickHandler(
+                googleLoginBtn.current,
+                {},
+                googleOkReturn,
+                (err) => alert(JSON.stringify(err, undefined, 2))
+            );
         });
     };
 
-    useEffect(googleSDK_init, [googleUser, auth2]);
-    const logTitle = auth2.isSignedIn?.get() ? "로그아웃" : "로그인";
+    const googleSDKLoaded = () => {
+        //구글 SDK 불러오기
+        (function (doc, script, id) {
+            let js;
+            const fjs = doc.getElementsByTagName(script)[0];
+            if (doc.getElementById(id)) {
+                return;
+            }
+            js = doc.createElement(script);
+            js.id = id;
+            js.src =
+                "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, "script", "google-jssdk");
+    };
+
+    useEffect(() => {
+        googleSDK_init();
+        googleSDKLoaded();
+    }, []);
+
+    // console.log(googleUser);
+    const logTitle = googleUser?.name ? googleUser?.name : "로그인";
 
     const googleLogout = (e) => {
-        // e.stopPropargation();
         const auth2 = window.gapi.auth2.getAuthInstance();
         auth2?.disconnect();
-        setAuth2("");
+        setGoogleUser("");
+        console.log("logout");
+        e.stopPropagation();
+        // e.stopImmediatePropagation();
         return;
     };
 
     return (
         <div id="gSignInWrapper">
-            <div
-                id="customBtn"
-                class="customGPlusSignIn"
-                ref={auth2.isSignedIn?.get() ? null : googleLoginBtn}
-                onClick={auth2.isSignedIn?.get() ? googleLogout : ""}
-            >
+            <div id="customBtn" class="customGPlusSignIn" ref={googleLoginBtn}>
                 <span class="icon"></span>
                 <span class="buttonText">Google {logTitle}</span>
             </div>
